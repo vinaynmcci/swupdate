693e76bb756b9b0409a410564db0ec939f2e25f9
vinaynmcci <vinayn@mcci.com>
2023-11-03 18:29:58 +0530

    initial

--- Brix/src/uiGlobal.py
+++ Brix/src/uiGlobal.py
@@ -40,7 +40,7 @@ VERSION_NAME  = "\nMCCI"+u"\u00AE"+" Brix UI"
 VERSION_ID    = ""
 VERSION_COPY  = "\nCopyright "+u"\u00A9"+" 2022 MCCI Corporation"
 
-VERSION_STR = "v3.1.0"
+VERSION_STR = "v3.2.0"
 
 repository_owner = "vinaynmcci"
 repository_name = "swupdate"
