02c06801e316062b0e41299d0f6fde682fbb17dd
vinaynmcci <vinayn@mcci.com>
2023-11-03 12:47:23 +0530

    initial commit

--- Brix/src/uiGlobal.py
+++ Brix/src/uiGlobal.py
@@ -40,7 +40,7 @@ VERSION_NAME  = "\nMCCI"+u"\u00AE"+" Brix UI"
 VERSION_ID    = ""
 VERSION_COPY  = "\nCopyright "+u"\u00A9"+" 2022 MCCI Corporation"
 
-VERSION_STR = "v1.4.0"
+VERSION_STR = "v1.5.0"
 
 repository_owner = "vinaynmcci"
 repository_name = "swupdate"
@@ -69,13 +69,14 @@ def check_version():
         if latest_version:
             if latest_version > VERSION_STR:
                 update_info.SetLabel(f"A new version ({latest_version}) is available! Click OK to update.")
+                dlg.SetSize(300, 150)
+                dlg.ShowModal()
+                dlg.Destroy()
             else:
                 update_info.SetLabel("You are using the latest version.")
-
-        dlg.SetSize(300, 150)
-        dlg.ShowModal()
-        dlg.Destroy()
-        app.MainLoop()
+                dlg.SetSize(300, 150)
+                dlg.ShowModal()
+                dlg.Destroy()
 
     else:
         print(f"Failed to retrieve information from GitHub. Status code: {response.status_code}")
